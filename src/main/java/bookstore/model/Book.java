4cd1cc730b3f875c2bd91bc522cbb70867257fe8
Tatyana Pomaz <pomazts@gmail.com>
2017-06-30 23:56:23 +0300

    add entity Author

--- src/main/java/bookstore/model/Book.java
+++ src/main/java/bookstore/model/Book.java
@@ -1,8 +1,16 @@
 package bookstore.model;
 
-import org.hibernate.annotations.ManyToAny;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
@@ -23,7 +31,7 @@ public class Book {
     private double price;
 
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name="book_author", joinColumns = @JoinColumn(name = "id"),
+    @JoinTable(name = "book_author", joinColumns = @JoinColumn(name = "id"),
             inverseJoinColumns = @JoinColumn(name = "id"))
     private Set<Author> authors;
 
